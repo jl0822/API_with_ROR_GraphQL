3ef534df082715e1d74bfd7cb59eab23b3048b22
jl0822 <jluicanada@gmail.com>
2020-04-26 09:42:27 +0900

    added tables for database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+
+author1 = Author.create(name: 'Arjunan', bio: 'Ruby developer')
+author2 = Author.create(name: 'David', bio: 'Angular developer')
+Article.create(
+title: 'Basics of Ruby Programming',
+description: 'This article is related to ruby programming',
+author: author1
+)
+Article.create(
+title: 'How to create Angular application',
+description: 'This article is related to Angular App',
+author: author2
+)
